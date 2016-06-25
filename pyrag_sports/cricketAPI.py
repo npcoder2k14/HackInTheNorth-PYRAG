358b9a282ac840c8b68906138dcccef31ab190ac
Mayank Pratap <mayank7860@gmail.com>
2016-06-26 00:10:58 +0530

    Trying to add CORS Headers

--- pyrag_sports/cricketAPI.py
+++ pyrag_sports/cricketAPI.py
@@ -8,9 +8,16 @@ try:
     from flask_cors import CORS, cross_origin
 
     app = Flask(__name__)
-    CORS(app)
+    def add_cors_headers(response):
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        if request.method == 'OPTIONS':
+            response.headers['Access-Control-Allow-Methods'] = 'DELETE, GET, POST, PUT'
+            headers = request.headers.get('Access-Control-Request-Headers')
+            if headers:
+                response.headers['Access-Control-Allow-Headers'] = headers
+        return response
+    app.after_request(add_cors_headers)
 
-    @app.route("/")
 except:
     pass
 
